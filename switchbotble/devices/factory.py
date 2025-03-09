7c702bf78c875bf86505069d46bf67730b3ad7b0
Yoshio HANAWA <y@hnw.jp>
2025-03-09 14:42:03 +0900

    feat: Support new device for Meter, Meter Pro, and Water Leak Detector

--- switchbotble/devices/factory.py
+++ switchbotble/devices/factory.py
@@ -3,6 +3,9 @@ from bleak.backends.scanner import BLEDevice, AdvertisementData
 from .base import SwitchBotDevice
 from .contact_sensor import ContactSensor
 from .motion_sensor import MotionSensor
+from .meter import Meter
+from .meter_pro import MeterPro
+from .water_leak_detector import WaterLeakDetector
 from .unknown_sensor import UnknownSensor
 
 class SwitchBotDeviceFactory(metaclass=ABCMeta):
@@ -13,6 +16,12 @@ class SwitchBotDeviceFactory(metaclass=ABCMeta):
             return ContactSensor(d, **kwargs)
         elif dev_type == 0x73:
             return MotionSensor(d, **kwargs)
+        elif dev_type == 0x54:
+            return Meter(d, **kwargs)
+        elif dev_type == 0x35:
+            return MeterPro(d, **kwargs)
+        elif dev_type == 0x26:
+            return WaterLeakDetector(d, **kwargs)
         else:
             if kwargs.get("debug"):
                 print(f"Unknown device type '{chr(dev_type)}'({hex(dev_type)})")
