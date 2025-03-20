d8b3744af2cf6625df2475089ae5fe633d7013db
Yoshio HANAWA <y@hnw.jp>
2025-03-20 16:45:48 +0900

    fix: correct motion_timeout being ignored / include push_count in status dict / rename some key in status dict/ revise private function names

--- switchbotble/devices/base.py
+++ switchbotble/devices/base.py
@@ -7,16 +7,20 @@ class SwitchBotDevice(metaclass=ABCMeta):
     def __init__(self, d: BLEDevice, debug = False, **kwargs):
         self.debug = debug
         self.d = d
+        self.prev_status = None
         self.status = None
 
     def update(self, ad: AdvertisementData):
-        current_status = self._parse_advertisement_data(ad)
-        if self.status == None:
-            self.status = current_status
+        status = self._parse_advertisement_data(ad)
+        if self.status == status:
+            return
+        self.prev_status = self.status
+        self.status = status
+        self._process_status()
+        if self.prev_status == None:
             self.publish("found")
         else:
-            self._check_status(current_status)
-            self.status = current_status
+            self._publish_signal()
 
     def publish(self, topic_name: str):
         sig = signal(topic_name)
@@ -26,10 +30,19 @@ class SwitchBotDevice(metaclass=ABCMeta):
     def _parse_advertisement_data(self, ad: AdvertisementData):
         pass
 
-    @abstractmethod
-    def _check_status(self, curr):
+    def _process_status(self):
+        pass
+
+    def _publish_signal(self):
         pass
 
     def log(self, message: str):
         if self.debug:
             print(f"{dt.now().isoformat()} {self.d.address} {message}")
+
+    def __str__(self):
+        if self.status:
+            status_str = ", ".join(f"{key}={value}" for key, value in self.status.items())
+            return f"{self.__class__.__name__}: {status_str}"
+        else:
+            return f"{self.__class__.__name__}: Status is None"
--- switchbotble/devices/meter.py
+++ switchbotble/devices/meter.py
@@ -20,9 +20,5 @@ class Meter(SwitchBotDevice):
             'humidity_alert': (data[3] & 0x30) >> 4,
         }
 
-    def _check_status(self, curr):
-        prev = self.status
-        self.log(f"{curr}")
-
-    def __str__(self):
-        return f"{self.__class__.__name__}: battery={self.battery}, light={self.light}, motion_raw={self.motion_raw}, motion={self.motion}, last_motion={self.last_motion}, contact={self.contact}, hal_utc={self.last_contact}, enter_count={self.enter_count}, exit_count={self.exit_count}, button_count={self.button_count}"
+    def _publish_signal(self):
+        pass
