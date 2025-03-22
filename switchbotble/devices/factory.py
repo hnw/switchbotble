e7130c87aa19742517be176778e9297c31b9ffce
Yoshio HANAWA <y@hnw.jp>
2025-03-22 11:34:49 +0900

    fix: Replace Enum with constants

--- switchbotble/devices/factory.py
+++ switchbotble/devices/factory.py
@@ -8,29 +8,28 @@ from .meter_pro import MeterPro
 from .water_leak_detector import WaterLeakDetector
 from .unknown_sensor import UnknownSensor
 
-class DeviceType(Enum):
-    # see: https://github.com/OpenWonderLabs/SwitchBotAPI-BLE#device-types
-    CONTACT_SENSOR = 0x64
-    MOTION_SENSOR = 0x73
-    METER = 0x54
-    METER_PRO = 0x35
-    WATER_LEAK_DETECTOR = 0x26
+# see: https://github.com/OpenWonderLabs/SwitchBotAPI-BLE#device-types
+CONTACT_SENSOR = 0x64
+MOTION_SENSOR = 0x73
+METER = 0x54
+METER_PRO = 0x35
+WATER_LEAK_DETECTOR = 0x26
 
 class SwitchBotDeviceFactory(metaclass=ABCMeta):
     @staticmethod
-    def create(dev_type: int, d: BLEDevice, **kwargs) -> SwitchBotDevice:
+    def create(device_type: int, d: BLEDevice, **kwargs) -> SwitchBotDevice:
         # see: https://github.com/OpenWonderLabs/SwitchBotAPI-BLE#device-types
-        if device_type == DeviceType.CONTACT_SENSOR:
+        if device_type == CONTACT_SENSOR:
             return ContactSensor(d, **kwargs)
-        elif device_type == DeviceType.MOTION_SENSOR:
+        elif device_type == MOTION_SENSOR:
             return MotionSensor(d, **kwargs)
-        elif device_type == DeviceType.METER:
+        elif device_type == METER:
             return Meter(d, **kwargs)
-        elif device_type == DeviceType.METER_PRO:
+        elif device_type == METER_PRO:
             return MeterPro(d, **kwargs)
-        elif device_type == DeviceType.WATER_LEAK_DETECTOR:
+        elif device_type == WATER_LEAK_DETECTOR:
             return WaterLeakDetector(d, **kwargs)
         else:
             if kwargs.get("debug"):
-                print(f"Unknown device type '{chr(dev_type)}'({hex(dev_type)})")
+                print(f"Unknown device type '{chr(device_type)}'({hex(device_type)})")
             return UnknownSensor(d, **kwargs)
